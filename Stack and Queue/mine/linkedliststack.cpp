df929f270b8ad41eea5ae16f857c9ebffbb76524
Abdulhafiz <abdulhafizaderemi@gmail.com>
2023-05-23 08:19:19 +0100

    first commit

--- Stack and Queue/mine/linkedliststack.cpp
+++ Stack and Queue/mine/linkedliststack.cpp
@@ -7,10 +7,49 @@ struct Node
     Node* next;
 };
 
-Node* head = NULL;
+Node* top = NULL;
 
-struct Stack
+void Push(int data) {
+    Node* temp = new Node;
+
+    temp->data = data;
+    temp->next = top;
+    top = temp;
+}
+
+void Pop() {
+    Node *temp;
+
+    if (top == NULL)
+    {
+        return;
+    }
+
+    temp = top;
+    top = top->next;
+    free(temp);
+}
+
+bool isEmpty()
 {
-    /* data */
-};
+    return top == NULL;
+}
 
+int peek()
+{
+    if (isEmpty())
+    {
+        exit(1);
+    } else
+    {
+        return top->data;
+    }
+}
+
+int main()
+{
+    Push(3);
+    Push(7);
+    Push(6);
+    peek();
+}
